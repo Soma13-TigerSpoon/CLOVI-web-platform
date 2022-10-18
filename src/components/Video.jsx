a4ab0e02d9e8a4bcf05d70f6b32d5e0a15eddd90
voka <chdlswhd7@naver.com>
2022-10-18 18:16:17 +0900

    Feat: react-youtube 패키지 설치 후  웹페이지에 유튜브 영상 삽입 후 기능 추가
    1. 영상의 현재 시간 감지 (useEffect 사용)
    2. 영상의 원하는 시점으로 이동 (useState 사용)

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,5 +1,8 @@
-function Video () {
-    return <h1>Video</h1>;
-}
+import YT from "./Youtube";
+import styled from "styled-components";
 
+function Video(){
+    console.log(YT)
+    return <YT></YT>;
+}
 export default Video;
---
+++ src/components/Youtube.jsx
@@ -0,0 +1,96 @@
+import {useState, useEffect} from 'react'
+import YouTube from 'react-youtube';
+
+function YT(){
+  const [player, setPlayer] = useState();
+  const [elapsed, setElapsed] = useState(0);
+  const [width, setWidth] = useState(600);
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const elapsed_sec = await player?.getCurrentTime(); // this is a promise. dont forget to await
+
+      // calculations
+      const elapsed_ms = Math.floor(elapsed_sec * 1000);
+      const ms = elapsed_ms % 1000;
+      const min = Math.floor(elapsed_ms / 60000);
+      const seconds = Math.floor((elapsed_ms - min * 60000) / 1000);
+
+      setElapsed(
+        min.toString().padStart(2, '0') +
+          ':' +
+          seconds.toString().padStart(2, '0') +
+          ':' +
+          ms.toString().padStart(3, '0'),
+      );
+    }, 100); // 100 ms refresh. increase it if you don't require millisecond precision
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  return <div className="player">
+      <div style={{ display: 'flex', marginBottom: '1em' }}>
+        <button type="button" onClick={() => player?.seekTo(120, true)}>
+          Seek to 2 minutes
+        </button><button type="button" onClick={() => console.log(elapsed)}>
+          print
+        </button>
+        <div>{elapsed}</div>
+      </div>
+
+      {(
+        <YouTube
+          videoId={'XxVg_s8xAms'}
+          opts={{
+            width,
+            height: width * (9 / 16),
+            playerVars: {
+              autoplay: 0,
+            },
+          }}
+          className="container"
+          onReady={(event) => setPlayer(event.target)}
+        />
+      )}
+    </div>
+}
+
+export function getPreItems(){
+    ;
+}
+export function getNextItems(){
+    ;
+}
+export default YT;
+// function YT(){
+//   const [elapsed, setElapsed] = useState(0);
+//   const playerRef = useRef();
+//   useEffect(() => {
+//     const interval = setInterval(async () => {
+//       const elapsed_sec = await playerRef.current.getCurrentTime(); // this is a promise. dont forget to await
+
+//       // calculations
+//       const elapsed_ms = Math.floor(elapsed_sec * 1000);
+//       const ms = elapsed_ms % 1000;
+//       const min = Math.floor(elapsed_ms / 60000);
+//       const seconds = Math.floor((elapsed_ms - min * 60000) / 1000);
+
+//       setElapsed(
+//         min.toString().padStart(2, '0') +
+//           ':' +
+//           seconds.toString().padStart(2, '0') +
+//           ':' +
+//           ms.toString().padStart(3, '0'),
+//       );
+//     }, 100); // 100 ms refresh. increase it if you don't require millisecond precision
+
+//     return () => {
+//       clearInterval(interval);
+//     };
+//   }, []);
+//   return (<div><div>
+//     <YouTubePlayer height={250} ref={playerRef} videoId={'DC471a9qrU4'}/>
+//     <text values={playerRef.current}></text>
+//     </div><button onClick={() => playerRef.current.seekTo(120, true)}></button>
+//     </div>);
+// }
