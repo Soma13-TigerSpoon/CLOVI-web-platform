a8a2dcc46cebdbcd2d9b0fafb17cdae939076870
voka <chdlswhd7@naver.com>
2022-10-20 18:11:16 +0900

    Feat: 영상 재생 시간에 맞춰 시간 인덱스가 바뀌는 기능 추가 (NTSP-249)

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,50 +1,56 @@
+import styled from "styled-components";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
+
 import Youtube from "./Youtube";
 import { getVideoInfoByURL } from "../service/video";
 let videoData;
-let curIndex;
+export let timeline;
 function Video(){
-    const [index, setIndex] = useState(10);
+    const [index, setIndex] = useState(-1);
     let {videoId} = useParams();
     console.log(videoId);
     useEffect(()=>{
         setItems(videoId);
     },[]);
     return <div id="youtube_controller">
-        <Youtube urlId={videoId} ></Youtube>
-        <DivBtn className='Btn_time'>
-            <button id="left" onClick={() => {
-                if(index!=0){
-                    setIndex(index-1);
-                    getItems(index);
-                }
-            }}>Left</button>
-            <button id="right" onClick={() => {
-                if(index!=videoData.lists.length){
-                    setIndex(index+1);
-                    getItems(index);
-                }
-            }}>Right</button>
-        </DivBtn>
+        <Youtube urlId={videoId} currentIndex = {index} setCurrentIndex = {setIndex}></Youtube>
     </div>
 }
-function getItems(curIndex){
 
+export function getItems(curIndex){
+    console.log(curIndex);
+    return videoData.lists[curIndex].items;
 }
 async function setItems(urlId){
     let result = await getVideoInfoByURL(urlId);
+    console.log(result);
     videoData = result;
-    console.log(videoData.lists.length);
+    timeline = [];
+    for(let i=0;i<result.lists.length;++i){
+        timeline.push(parseInt(result.lists[i].times.start));
+    }
+    timeline.sort((a, b) => {return a - b;});
+    console.log(timeline);
 }
-
+export function getCurrentIndexByTime(currentItemId,currentTime){
+    let newCurrentItemId;
+    if (currentTime < timeline[0]) {
+        newCurrentItemId = -1;
+    } else {
+        for (let i = timeline.length - 1; i > -1; i--) {
+            if (currentTime >= timeline[i]) {
+                newCurrentItemId = i;
+                break;
+            }
+        }
+    }
+    if (newCurrentItemId != currentItemId) {
+        currentItemId = newCurrentItemId;
+    }
+    return currentItemId;
+}
 export default Video;
 
 
-const DivBtn = styled.div`
-    width: 600px;
-    display : flex;
-    justify-content: space-between;
-`
 
--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -1,42 +1,24 @@
+import styled from "styled-components";
 import {useState, useEffect} from 'react'
+import { getCurrentIndexByTime, getItems, timeline } from './Video';
 import YouTube from 'react-youtube';
-function Youtube({ urlId }){
+function Youtube({ urlId, currentIndex, setCurrentIndex }){
   const [player, setPlayer] = useState();
   const [elapsed, setElapsed] = useState(0);
   const width = 600;
   useEffect(() => {
     const interval = setInterval(async () => {
-      const elapsed_sec = await player?.getCurrentTime(); // this is a promise. dont forget to await
-
-      // calculations
-      const elapsed_ms = Math.floor(elapsed_sec * 1000);
-      const ms = elapsed_ms % 1000;
-      const min = Math.floor(elapsed_ms / 60000);
-      const seconds = Math.floor((elapsed_ms - min * 60000) / 1000);
-
-      setElapsed(
-        min.toString().padStart(2, '0') +
-          ':' +
-          seconds.toString().padStart(2, '0') +
-          ':' +
-          ms.toString().padStart(3, '0'),
-      );
-    }, 100); // 100 ms refresh. increase it if you don't require millisecond precision
-
+      const elapsed_sec = await Math.floor(player?.getCurrentTime()); // this is a promise. dont forget to await
+      setElapsed(elapsed_sec);
+      setCurrentIndex(getCurrentIndexByTime(currentIndex,elapsed_sec));
+    }, 1000); // 100 ms refresh. increase it if you don't require millisecond precision
     return () => {
       clearInterval(interval);
     };
   }, [player]);
   
   return <div className="player">
-      <div style={{ display: 'flex', marginBottom: '1em' }}>
-        <button type="button" onClick={() => player?.seekTo(120, true)}>
-          Seek to 2 minutes
-        </button><button type="button" onClick={() => console.log(elapsed)}>
-          print
-        </button>
-        <div>{elapsed}</div>
-      </div>
+      
 
       {(
         <YouTube
@@ -52,10 +34,37 @@ function Youtube({ urlId }){
           onReady={(event) => setPlayer(event.target)}
         />
       )}
+      <div style={{ display: 'flex', marginBottom: '1em' }}>
+        <div>Current Time : {elapsed}</div>
+        <div>Current Index : {currentIndex}</div>
+      </div>
+      <DivBtn className='Btn_time'>
+            <button id="left" onClick={() => {
+                if(currentIndex!=0){
+                  console.log(currentIndex-1);
+                  player?.seekTo(timeline[currentIndex-1], true);
+                  console.log(currentIndex, timeline[currentIndex]);
+                }
+            }}>Left</button>
+            <button id="right" onClick={() => {
+                if(currentIndex!=timeline.length-1){
+                  player?.seekTo(timeline[currentIndex+1], true);
+                  console.log(currentIndex, timeline[currentIndex]);
+                }
+            }}>Right</button>
+        </DivBtn>
     </div>
 }
 
 export default Youtube;
+
+
+const DivBtn = styled.div`
+    width: 600px;
+    display : flex;
+    justify-content: space-between;
+`
+
 // function YT(){
 //   const [elapsed, setElapsed] = useState(0);
 //   const playerRef = useRef();
