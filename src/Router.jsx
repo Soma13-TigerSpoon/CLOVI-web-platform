276f4c104a7ed85a7dd59d87ae06d842ee73de1a
khhan <jacob2572502@gmail.com>
2022-11-15 18:26:12 +0900

    Feat: channel page, mobile UI Resolves: NTSP-363

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Channel from "./components/Channel";
 import Home from "./components/Home";
 import Item from "./components/Item";
 import Video from "./components/Video";
@@ -10,6 +11,7 @@ function Router() {
         <Route path="/" element={<Navigate to="/list/items" />}></Route>
         <Route path="/list" element={<Navigate to="/list/items" />}></Route>
         <Route path="/list/*" element={<Home />}></Route>
+        <Route path="/channel/:channelId" element={<Channel />}></Route>
         <Route path="/item/:itemId" element={<Item />}></Route>
         <Route path="/video/:videoId" element={<Video />}></Route>
       </Routes>
